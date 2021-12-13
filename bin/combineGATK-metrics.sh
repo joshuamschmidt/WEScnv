9fcd32de760f852270f0dcd6002b2b4e875cf2c1
joshuamschmidt <joshmschmidt1@gmail.com>
2021-12-13 16:22:05 +1030

    moddified collectMetrics sh script

--- bin/combineGATK-metrics.sh
+++ bin/combineGATK-metrics.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-sample_id=${1}
-hsMetrics=${2}
-isMetrics=${3}
+#sample_id=${1}
+hsMetrics=${1}
+isMetrics=${2}
 
 
 HS=($(sed '8q;d' "$hsMetrics" | cut -f9,32,46,48,52,63,64));
-IS=$(sed '8q;d' "$isMetrics" | cut -f6);
+IS=$(sed '8q;d' "$isMetrics" | cut -f1,3,6,7);
 OUT=($sample "${HS[@]}" "$IS");
 echo "${OUT[*]}" | tr ' ' '\t'
