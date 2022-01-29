ab3f8d7d64c866162dfcf11cc9a338ff9ddc57fa
joshuamschmidt <joshmschmidt1@gmail.com>
2022-01-30 01:02:23 +1030

    added exclusion of samples with non XX or XY karyotypes

--- bin/ExomeDepthX.R
+++ bin/ExomeDepthX.R
@@ -4,7 +4,7 @@ pacman::p_load('data.table','ExomeDepth',install = F, update = F)
 # from ARGV
 counts <- fread(args[1],header=T)
 setnames(counts, old=c("CHR","START","END"), new=c("chromosome","start","end"))
-counts <- counts[chromosome == "X"]
+counts <- counts[chromosome == "chrX"]
 counts <- counts[!MAP100 %in% NaN]
 counts <- counts[order(chromosome,start,end)]
 # test and reference exomes
@@ -16,6 +16,14 @@ candidate_ref_set <- test_candidate_ref_set[!sample_id %in% test_sample,sample_i
 # bioSex of test and references
 bio_sex <- fread(args[3], header=T)
 test_sex <- bio_sex[Sample_ID==test_sample,bioSex]
+if(test_sex!="FEMALE:XX" & test_sex!="MALE:XY") {
+  no_run <- data.table(calls="None")
+  outfile <- paste(test_sample,"ExomeDepth-CNV.calls.bed",sep="_")
+  fwrite(no_run,outfile,sep="\t",col.names = F, row.names = F, quote = F)
+  outfile <- paste(test_sample,"ExomeDepth-CNV.reference.txt",sep="_")
+  write.table(x=no_run$calls,file=outfile,sep="\n",col.names = F, row.names = F, quote = F)
+  quit(save = "no", status = 0, runLast = FALSE)
+}
 candidate_ref_set <- bio_sex[bioSex==test_sex][Sample_ID %in% candidate_ref_set,Sample_ID]
 
 test.exome <- counts[,get(test_sample)]
@@ -25,7 +33,7 @@ my.choice <- select.reference.set(test.counts = test.exome,
                                   bin.length = (counts$end - counts$start)/1000,
                                   data = counts[,.(MAP100,GC,GC500,WIDTH)],
                                   formula = 'cbind(test, reference) ~ 1 + MAP100 + GC+ GC500 + WIDTH',
-                                  n.bins.reduced = 5000)
+                                  n.bins.reduced = 1000)
 
 best_ref_set <- my.choice$reference.choice
 n_ref_set <- length(best_ref_set)
